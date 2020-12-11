11234ade015fb2b31fc7a909ae703b7ba0c80de8
Kalata777 <72138487+Kalata777@users.noreply.github.com>
2020-12-11 17:05:15 +0200

    Update Weapon.java

--- Weapon.java
+++ Weapon.java
@@ -4,7 +4,7 @@ public class Weapon {
 int damage;
 int ammo;
 String name;
-	 public Weapon(int damage, int ammo, String name) {
+	  Weapon(int damage, int ammo, String name) {
 		 this.damage=damage;
 		 this.ammo=ammo;
 		 this.name=name;
